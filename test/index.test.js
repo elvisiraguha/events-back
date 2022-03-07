1333e22312e3224979c8f0f66093e985c996c5a9
Elvis Iraguha <iraguhaelvis@gmail.com>
2022-03-07 11:59:59 +0200

    configure tests and github actions

---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+import mocha, { describe, it } from "mocha";
+import assert from "assert";
+
+describe("1st", () => {
+  it("should return -1 when the value is not present", () => {
+    assert.equal([1, 2, 3].indexOf(4), -1);
+  });
+});
